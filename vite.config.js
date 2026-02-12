2017fb80d1ef43220b515a9dfb6d21a7f656c011
$ῗἧḡḥ𝐀丂𝓱м𝑒𝑒𝐓 <91828139+ashmeet07@users.noreply.github.com>
2026-02-12 22:01:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    monacoEditorPlugin(),   // 🔥 THIS FIXES EVERYTHING
-  ],
+  plugins: [react()],
+
+  base: "/",   // ✅ Important for Vercel deployment
+
+  server: {
+    port: 5173,     // optional (default)
+    open: true      // optional
+  },
+
+  build: {
+    outDir: "dist", // default but good to be explicit
+  }
 });
